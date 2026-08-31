9863bc2277cd448b0a3441b4590a41ed1fce2ded
Harsh kumar chaubey <harshchaubey2020@gmail.com>
2026-08-31 11:13:13 +0530

    update file

---
+++ monday.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main(){
+
+    printf("\tMONDDAY\n");
+    printf("------------------------\n");
+    printf("09:00\t-\t10:00\t:\tc\n");
+    printf("10:00\t-\t11:00\t:\tMaths\n");
+    printf("11:00\t-\t12:00\t:\tphysics\n");
+    printf("12:00\t-\t01:00\t:\tbreak\n");
+    printf("------------------------\n");
+
+}
---
+++ product.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int product;
+    product = 101;
+char first = 'L';
+float price = 499.50;
+
+printf("Product ID\t\t:\t%d\n",product);
+printf("Product Code\t\t:\t%c\n",first);
+printf("Price\t\t\t:\t%.2f",price);
+
+return 0;
+
+
+}
