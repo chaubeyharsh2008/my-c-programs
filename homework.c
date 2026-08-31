fcce3c731587794d116122f242b96f73b7ee3f94
Harsh kumar chaubey <harshchaubey2020@gmail.com>
2026-08-31 10:45:55 +0530

    update code

---
+++ homework.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main(){
+    int age;
+    age = 18;
+   float percentage = 84.5;
+   char section = 'A';
+   char first = 'R';
+
+   printf("------------------------\n");
+
+   printf("\tSTUDENT CARD\n");
+
+   printf("------------------------\n");
+
+
+   printf("first\t\t: %c\n",first);
+
+   printf("Age\t\t: %d\n",age);
+
+   printf("percentage\t: %.2f\n",percentage);
+
+   printf("section\t\t: %c\n",section);
+
+   printf("------------------------\n");
+
+
+   return 0;
+
+
+   
+
+
+     
+
+}
